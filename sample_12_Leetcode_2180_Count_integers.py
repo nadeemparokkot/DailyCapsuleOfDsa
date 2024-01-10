22b5fb0d38a4b598936c357e8043cde7a3c4dd55
Nadeem Parokkot <122343207+nadeemparokkot@users.noreply.github.com>
2024-01-10 15:09:11 +0530

    Add files via upload

---
+++ sample_12_Leetcode_2180_Count_integers.py
@@ -0,0 +1,33 @@
+"""
+Given a positive integer num, return the number of positive integers less than or equal to num whose digit sums are even.
+
+The digit sum of a positive integer is the sum of all its digits.
+
+
+
+Example 1:
+
+Input: num = 4
+Output: 2
+Explanation:
+The only integers less than or equal to 4 whose digit sums are even are 2 and 4.
+Example 2:
+
+Input: num = 30
+Output: 14
+Explanation:
+The 14 integers less than or equal to 30 whose digit sums are even are
+2, 4, 6, 8, 11, 13, 15, 17, 19, 20, 22, 24, 26, and 28.
+"""
+
+class Solution:
+    def countEven(self, num: int) -> int:
+        count=0
+        for n in range(1,num+1):
+            digit=str(n)
+            digitsum=0
+            for i in range(len(digit)):
+                digitsum+=int(digit[i])
+            if digitsum%2==0:
+                count+=1
+        return count
